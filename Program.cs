e2627ac5980c038d74bfccdd77191ef5215453fc
Awaluda <iqbalawaluda@students.amikom.ac.id>
2021-03-18 17:21:09 +0700

    tugas 2

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menmbuat_ClassObjectTaxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi() ;
+
+            taxi.DriverName = "Awal";
+            taxi.OnDuty = false;
+            taxi.NumPassenger = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menmbuat_ClassObjectTaxi
+{
+    class Taxi
+    {
+      public string DriverName { get; set; }
+      public bool OnDuty { get; set; }
+      public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name         :{0}", DriverName);
+
+            if(OnDuty == true) 
+            {
+                Console.WriteLine("On Duty             :Yes", OnDuty);
+            }
+            else 
+            {
+                Console.WriteLine("On Duty             :No", OnDuty);
+            }
+
+            Console.WriteLine("Number Of Passenger :{0}\n", NumPassenger);
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", NumPassenger);
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesi menjemput penumpang\n", DriverName);
+        }
+    }
+}
